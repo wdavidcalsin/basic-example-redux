f68a41981e1dcde3a13b6db3b7b65ff8000a09d7
WDavid Calsin <wdavidcalsin968@gmail.con>
2021-03-11 22:08:23 -0500

    refactor: 💡 refactor ADD and MINUS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Counter } from "./features/counter/Counter";
-import "./App.css";
+import "./styles/index.scss";
 
 function App() {
   return (
--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+
+import { BsPlus } from "react-icons/bs";
+import { BiMinus } from "react-icons/bi";
+
 import { RootState } from "../../reducers";
-import { increment } from "../../actions/counterActions";
+import { decrement, increment } from "../../actions/counterActions";
 
 export function Counter() {
   const count = useSelector((state: RootState) => state.counter);
@@ -12,12 +16,18 @@ export function Counter() {
   });
 
   return (
-    <div>
-      <h1>Willian david</h1>
-      <h3>Count: {count}</h3>
-      <button type="button" onClick={() => dispatch(increment())}>
-        Click
-      </button>
+    <div className="counter">
+      <div className="item">
+        <h3>COUNT: {count}</h3>
+        <div className="btn">
+          <button type="button" onClick={() => dispatch(increment())}>
+            <BsPlus />
+          </button>
+          <button type="button" onClick={() => dispatch(decrement())}>
+            <BiMinus />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
